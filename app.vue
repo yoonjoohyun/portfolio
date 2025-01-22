9e06e4c83b1cc61a22a60568f60b434128413629
yoonjoohyun <crayon_000@optten.co.kr>
2025-01-22 12:51:39 +0900

    setting

--- app.vue
+++ app.vue
@@ -4,16 +4,6 @@
 </template>
 
 <script setup>
-// 로그인 상태를 관리하기 위한 composable 사용
-const auth = useState('auth', () => false)
-
-// 로그인하지 않은 상태에서 main 페이지 접근 방지
-onMounted(() => {
-  const route = useRoute()
-  if (!auth.value && route.path !== '/login') {
-    navigateTo('/login')
-  }
-})
 </script>
 
 <style lang="scss" src="@/assets/scss/app.scss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,9 @@
 <template>
 <div class="main_bg">
-    <button @click="handleLogout" class="logout_btn">Logout</button>
+    
 </div>
 </template>
 <script setup>
-
-const auth = useState('auth')
-
-// 로그인하지 않은 상태에서 접근 시 로그인 페이지로 리다이렉션
-onMounted(() => {
-  if (!auth.value) {
-    navigateTo('/login')
-  }
-})
-
-const handleLogout = () => {
-  auth.value = false
-  navigateTo('/login')
-}
-
 </script>
 
 <style lang="scss" scoped src="~/assets/scss/main.scss"></style>
