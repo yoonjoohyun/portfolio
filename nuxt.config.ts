c6c6ba1333239d8b23f13e56a52995a88cd499a9
yoonjoohyun <crayon_000@optten.co.kr>
2025-02-05 01:58:31 +0900

    0205

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,16 +6,12 @@ export default defineNuxtConfig({
     head: {
       link: [
         {
-          // Font:min-sans
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/MinSans-Regular.woff'
-        },
-        {
           // Remix-icon
           rel: 'stylesheet',
           href: 'https://cdn.jsdelivr.net/npm/remixicon@4.1.0/fonts/remixicon.css'
         }
       ]
     }
-  }
+  },
+  css: ['~/assets/scss/fonts.scss']
 })
